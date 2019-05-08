5bfb16cab87ffbe9d1c50a8e38d0074c90a83466
Anliec <nicolas.six@pau.fr>
2019-05-08 18:12:35 -0400

    update loss function and data loading

--- load_yolo_data.py
+++ load_yolo_data.py
@@ -6,12 +6,12 @@ import numpy as np
 import itertools
 
 
-def load_yolo_gt(file_path: str, out_shape, class_to_load=(0,)):
+def load_yolo_gt(file_path: str, out_shape, class_to_load=("0",)):
     scores = np.zeros(out_shape)
     sizes = np.zeros(out_shape)
     with open(file_path, 'r') as gt:
         for line in gt:
-            vals = line.split(",")
+            vals = line.split(" ")
             if vals[0] in class_to_load:
                 x, y, w, h = [float(v) for v in vals[1:]]
                 x, w = [int(round(v * out_shape[0])) for v in [x, w]]
@@ -23,7 +23,6 @@ def load_yolo_gt(file_path: str, out_shape, class_to_load=(0,)):
 
 def load_yolo_image(image_path: str, input_size):
     im = cv2.imread(image_path)
-    print("original shape {} resized in {}".format(im.shape, input_size))
     im = cv2.resize(im, input_size)
     im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     im = im.astype(np.float)
@@ -31,32 +30,34 @@ def load_yolo_image(image_path: str, input_size):
     return im
 
 
-def load_yolo_pair(path_to_image: str, input_size):
+def load_yolo_pair(path_to_image: str, image_shape, annotation_shape):
     path_to_annotation = os.path.splitext(path_to_image)[0] + ".txt"
-    scores, sizes = load_yolo_gt(path_to_annotation, input_size)
-    im = load_yolo_image(path_to_image, tuple(input_size))
+    scores, sizes = load_yolo_gt(path_to_annotation, annotation_shape)
+    im = load_yolo_image(path_to_image, tuple(image_shape[::-1]))
     return im, scores, sizes
 
 
 def load_yolo_pair_for_map(args):
-    return load_yolo_pair(args[0], args[1])
+    return load_yolo_pair(args[0], args[1], args[2])
 
 
-def load_annotations_from_dir(dir_path: str, input_shape, images_regex: str = "*.jpg"):
+def load_annotations_from_dir(dir_path: str, image_shape, annotation_shape, images_regex: str = "*.jpg"):
     images = glob.glob(os.path.join(dir_path, images_regex))
     print("{} images found in {}".format(len(images), dir_path))
-    x = np.zeros(shape=(len(images),) + tuple(input_shape) + (3,), dtype=np.float)
-    y_score = np.zeros(shape=(len(images),) + tuple(input_shape), dtype=np.float)
-    y_size = np.zeros(shape=(len(images),) + tuple(input_shape), dtype=np.float)
+    x = np.zeros(shape=(len(images),) + tuple(image_shape) + (3,), dtype=np.float)
+    y_score = np.zeros(shape=(len(images),) + tuple(annotation_shape) + (1,), dtype=np.float)
+    y_size = np.zeros(shape=(len(images),) + tuple(annotation_shape) + (1,), dtype=np.float)
 
     pool = multiprocessing.Pool()
 
-    dataloader = pool.imap_unordered(load_yolo_pair_for_map, zip(images, itertools.repeat(input_shape)), chunksize=25)
+    dataloader = pool.imap_unordered(load_yolo_pair_for_map,
+                                     zip(images, itertools.repeat(image_shape), itertools.repeat(annotation_shape)),
+                                     chunksize=25)
 
     for i, (im, score, size) in enumerate(dataloader):
         x[i, :, :, :] = im
-        y_score[i, :, :] = score
-        y_size[i, :, :] = size
+        y_score[i, :, :, 0] = score
+        y_size[i, :, :, 0] = size
 
     return x, (y_score, y_size)
 
--- loss.py
+++ loss.py
@@ -1,34 +1,38 @@
 from keras import backend as K
+import numpy as np
 
 from utils import gaussian_kernel
 
 
-def detection_loss(gaussian_area: int = 31, score_tp_weight: float = 0.9, score_fp_weight: float = 0.5,
+def detection_loss(gaussian_diameter: int = 31, score_tp_weight: float = 0.9, score_fp_weight: float = 0.5,
                    score_weight: float = 0.5, size_weight: float = 0.5):
-    def custom_loss(y_true, y_pred):
-        y_pred_score, y_pred_size = y_pred
-        y_true_score, y_true_size = y_true
-    
-        g = gaussian_kernel(gaussian_area)
-        g /= g.max()
-    
-        score_upper_bound_target = K.conv2d(y_true_score, g)
-        score_upper_error = K.clip(K.sum(y_pred_score, -score_upper_bound_target), 0.0, 1.0)
-    
-        score_lower_error = K.clip(K.sum(-y_pred_score, y_true_score), 0.0, 1.0)
-    
+    g = np.zeros(shape=(gaussian_diameter, gaussian_diameter, 1, 1), dtype=np.float32)
+    g[:, :, 0, 0] = gaussian_kernel(gaussian_diameter)
+    g /= g.max()
+    g = K.constant(g, dtype=np.float32)
+
+    def score_loss(y_true, y_pred):
+        score_upper_bound_target = K.conv2d(y_true, g, padding="same")
+        # compute error for false detection (FP)
+        score_upper_error = K.clip(y_pred - score_upper_bound_target, 0.0, 1.0)
+        # compute error for detection (TP)
+        score_lower_error = K.clip(y_true - y_pred, 0.0, 1.0)
+
         score_upper_mse = K.mean(K.square(score_upper_error))
-        score_lower_mse = K.mean(K.square(score_lower_error))
+        score_lower_mse = K.sum(K.square(score_lower_error)) / (K.sum(y_true) + 1)
         score_mse = score_tp_weight * score_lower_mse + score_fp_weight * score_upper_mse
-    
-        size_mask = K.greater(y_true_size, 0.0)
-        size_pred_masked = size_mask * y_pred_size
-        size_error = K.sum(-size_pred_masked, y_true_size)
-        size_mse = K.mean(K.square(size_error))
+
+        return score_mse
+
+    def size_loss(y_true, y_pred):
+        # size_mask = K.cast(K.greater(y_true, 0.0), K.floatx())
+        # size_pred_masked = size_mask * y_pred
+        size_error = K.clip(y_true - y_pred, 0.0, 10000.0)
+        size_mse = K.square(size_error)
         
-        return score_mse * score_weight + size_mse * size_weight
+        return K.max(size_mse)
     
-    return custom_loss
+    return {"Score": score_loss, "Size": size_loss}
 
 
 
