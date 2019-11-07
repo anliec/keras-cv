a959fd2c85407971e4431b86159411bb47fa13b4
Anliec <nicolas.six@pau.fr>
2019-11-07 18:29:24 -0500

    fix flop compatibility with tf2.0

--- grid_search.py
+++ grid_search.py
@@ -53,12 +53,12 @@ def generate_combinations():
 
 # from https://stackoverflow.com/questions/49525776/how-to-calculate-a-mobilenet-flops-in-keras
 def get_flops(model):
-    run_meta = tf.RunMetadata()
-    opts = tf.profiler.ProfileOptionBuilder.float_operation()
+    run_meta = tf.compat.v1.RunMetadata()
+    opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
 
     # We use the Keras session graph in the call to the profiler.
-    flops = tf.profiler.profile(graph=K.get_session().graph,
-                                run_meta=run_meta, cmd='op', options=opts)
+    flops = tf.compat.v1.profiler.profile(graph=tf.compat.v1.keras.backend.get_session().graph,
+                                          run_meta=run_meta, cmd='op', options=opts)
 
     return flops.total_float_ops  # Prints the "flops" of the model.
 
